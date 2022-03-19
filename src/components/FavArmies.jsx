850da079532a65115a0a50fd0bee7fdc7f12323a
Jon Moliner <jonmolinerzama@gmail.com>
2022-03-19 13:14:07 +0100

    fix favarmies

--- src/components/FavArmies.jsx
+++ src/components/FavArmies.jsx
@@ -19,6 +19,7 @@ function FavArmies() {
       .catch(error => {
         console.log(error);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   console.log(armies);
 
